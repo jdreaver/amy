9b8ee58498e4a7b6d09c56fcb8bc7465a3100f81
David Reaver <johndreaver@gmail.com>
2018-04-29 17:05:38 -0700

    Write pretty printer for Core AST

---
+++ library/Amy/Core.hs
@@ -0,0 +1,6 @@
+module Amy.Core
+  ( module X
+  ) where
+
+import Amy.Core.AST as X
+import Amy.Core.Desugar as X
---
+++ library/Amy/Core/Pretty.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Amy.Core.Pretty
+  ( prettyModule
+  , prettyExpr
+  ) where
+
+import Data.Foldable (toList)
+
+import Amy.Core.AST
+import Amy.Literal
+import Amy.Pretty
+
+mkPrettyType :: Type -> PrettyType ann
+mkPrettyType (TyCon name) = PTyDoc $ pretty $ typeNameText name
+mkPrettyType (TyVar name) = PTyDoc $ pretty $ typeNameText name
+mkPrettyType (TyFun ty1 ty2) = PTyFun (mkPrettyType ty1) (mkPrettyType ty2)
+
+mkPrettyScheme :: Scheme -> PrettyScheme ann
+mkPrettyScheme (Forall vars ty) = PForall (pretty . typeNameText <$> vars) (mkPrettyType ty)
+
+prettyModule :: Module -> Doc ann
+prettyModule (Module bindings externs) =
+  vcatTwoHardLines $ (prettyExtern' <$> externs) ++ (prettyBinding' <$> bindings)
+
+prettyExtern' :: Extern -> Doc ann
+prettyExtern' (Extern name ty) =
+  prettyExtern (prettyIdent name) (mkPrettyType ty)
+
+prettyBinding' :: Binding -> Doc ann
+prettyBinding' (Binding ident scheme args _ body) =
+  prettyScheme' ident scheme <>
+  hardline <>
+  prettyBinding (prettyIdent ident) (prettyTypedIdent <$> args) (prettyExpr body)
+
+prettyScheme' :: Ident -> Scheme -> Doc ann
+prettyScheme' ident scheme = prettyBindingScheme (prettyIdent ident) (mkPrettyScheme scheme)
+
+prettyTypedIdent :: Typed Ident -> Doc ann
+prettyTypedIdent (Typed ty ident) = parens $ prettyIdent ident <+> "::" <+> prettyType (mkPrettyType ty)
+
+prettyIdent :: Ident -> Doc ann
+prettyIdent (Ident name _ _) = pretty name
+
+prettyExpr :: Expr -> Doc ann
+prettyExpr (ELit lit) = pretty $ showLiteral lit
+prettyExpr (EVar (Typed _ var)) = prettyIdent var
+prettyExpr (ECase (Case scrutinee matches)) =
+  prettyCase (prettyExpr scrutinee) (toList $ mkMatch <$> matches)
+ where
+  mkMatch (Match pat body) = (prettyPattern pat, prettyExpr body)
+prettyExpr (ELet (Let bindings body)) =
+  prettyLet (prettyBinding' <$> bindings) (prettyExpr body)
+prettyExpr (EApp (App f args _)) = sep $ prettyExpr f : (prettyExpr <$> toList args)
+prettyExpr (EParens expr) = parens $ prettyExpr expr
+
+prettyPattern :: Pattern -> Doc ann
+prettyPattern (PatternLit lit) = pretty $ showLiteral lit
+prettyPattern (PatternVar (Typed _ var)) = prettyIdent var
