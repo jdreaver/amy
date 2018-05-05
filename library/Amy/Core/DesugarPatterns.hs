7912da52119f555ad3cf89338249b35dcb9954ef
David Reaver <johndreaver@gmail.com>
2018-05-05 08:04:43 -0700

    Simplify pattern desugaring again (still not there)

--- library/Amy/Core/DesugarPatterns.hs
+++ library/Amy/Core/DesugarPatterns.hs
@@ -1,93 +1,62 @@
 module Amy.Core.DesugarPatterns
-  ( groupMatches
+  ( GroupedMatches(..)
+  , groupMatches
   ) where
 
-import Data.List (sortOn)
 import Data.List.NonEmpty (NonEmpty(..))
-import qualified Data.List.NonEmpty as NE
-import Data.Maybe (mapMaybe)
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Monoid ((<>))
 
-import Amy.Core.AST as C
 import Amy.TypeCheck.AST as T
 
 data GroupedMatches
   = GroupedMatches
-  { groupedMatchesLiterals :: ![LitMatchGroup]
-  , groupedMatchesConstructors :: ![ConsMatchGroup]
-  , groupedMatchesDefault :: !(Maybe DefaultMatch)
+  { groupedMatchesLiterals :: ![(Literal, T.Expr)]
+    -- TODO: This map doesn't preserve the order of the constructors. Bad!
+  , groupedMatchesConstructors :: !(Map T.ConstructorName (NonEmpty (Maybe T.Pattern, T.Expr)))
+  , groupedMatchesDefault :: !(Maybe (T.Typed T.Ident, T.Expr))
   } deriving (Show, Eq)
 
-data LitMatchGroup
-  = LitMatchGroup
-  { litMatchGroupLiteral :: !Literal
-  , litMatchGroupBody :: !T.Expr
-  } deriving (Show, Eq)
+groupedMatches :: GroupedMatches
+groupedMatches =
+  GroupedMatches
+  { groupedMatchesLiterals = []
+  , groupedMatchesConstructors = Map.empty
+  , groupedMatchesDefault = Nothing
+  }
 
-data ConsMatchGroup
-  = ConsMatchGroup
-  { consMatchGroupConstructor :: !(T.Typed T.ConstructorName)
-  , consMatchGroupArgs :: ![(Maybe T.Pattern, T.Expr)]
-  } deriving (Show, Eq)
+addConstructorMatch
+  :: T.ConstructorName
+  -> Maybe T.Pattern
+  -> T.Expr
+  -> GroupedMatches
+  -> GroupedMatches
+addConstructorMatch consName mArg body grouped =
+  grouped
+  { groupedMatchesConstructors =
+    Map.insertWith
+      (flip (<>))
+      consName
+      ((mArg, body) :| [])
+      (groupedMatchesConstructors grouped)
+  }
 
-data DefaultMatch
-  = DefaultMatch
-  { defaultMatchIdent :: !(T.Typed T.Ident)
-  , defaultMatchBody :: !T.Expr
-  } deriving (Show, Eq)
-
--- | Groups 'T.Match'es so we can desugar any nested patterns.
 groupMatches :: [T.Match] -> GroupedMatches
-groupMatches matches =
-  let
-    -- Extract the different types of matches we can have
-    mDefaultMatch = findDefaultMatch matches
-    litMatches = mapMaybe litMatch matches
-    consMatches = mapMaybe consMatch matches
+groupMatches = flip groupMatches' groupedMatches
 
-    -- Group constructor matches by constructor name
-    groupedByCons :: [NonEmpty (T.Typed T.ConstructorName, (Maybe T.Pattern, T.Expr))]
-    groupedByCons = NE.group $ sortOn fst consMatches
-
-    -- For each constructor, make a ConsMatchGroup
-    mkConsMatchGroup :: NonEmpty (T.Typed T.ConstructorName, (Maybe T.Pattern, T.Expr)) -> ConsMatchGroup
-    mkConsMatchGroup matches' =
-      let
-        consName = fst . NE.head $ matches'
-        args = NE.toList $ snd <$> matches'
-      in ConsMatchGroup consName args
-  in
-    GroupedMatches
-    { groupedMatchesLiterals = litMatches
-    , groupedMatchesConstructors = mkConsMatchGroup <$> groupedByCons
-    , groupedMatchesDefault = mDefaultMatch
-    }
-
-findDefaultMatch :: [T.Match] -> Maybe DefaultMatch
-findDefaultMatch matches =
-  let
-    defaultMatches = mapMaybe defaultMatch matches
-  in
-    case defaultMatches of
-      [] -> Nothing
-      [x] -> Just x
-      -- TODO: Make this a warning somehow instead of an error.
-      _ -> error $ "Found multiple default matches in " ++ show matches
-
--- | A default match is a match for a simple variable or a wildcard.
-defaultMatch :: T.Match -> Maybe DefaultMatch
-defaultMatch (T.Match pat body) =
+groupMatches' :: [T.Match] -> GroupedMatches -> GroupedMatches
+groupMatches' [] grouped = grouped
+groupMatches' (T.Match pat body : rest) grouped =
   case pat of
-    T.PVar ident -> Just (DefaultMatch ident body)
-    _ -> Nothing
-
-litMatch :: T.Match -> Maybe LitMatchGroup
-litMatch (T.Match pat body) =
-  case pat of
-    T.PLit lit -> Just $ LitMatchGroup lit body
-    _ -> Nothing
-
-consMatch :: T.Match -> Maybe (T.Typed T.ConstructorName, (Maybe T.Pattern, T.Expr))
-consMatch (T.Match pat body) =
-  case pat of
-    T.PCons (T.PatCons consName mArg _) -> Just (consName, (mArg, body))
-    _ -> Nothing
+    T.PLit lit ->
+      groupMatches' rest
+      $ grouped { groupedMatchesLiterals = groupedMatchesLiterals grouped ++ [(lit, body)] }
+    T.PVar var ->
+      -- N.B. Terminate here because we found a catch-all pattern
+      -- TODO: Warn if there are multiple catch-all patterns
+      grouped { groupedMatchesDefault = Just (var, body) }
+    T.PCons (T.PatCons (Typed _ consName) mArg _) ->
+      groupMatches' rest
+      $ addConstructorMatch consName mArg body grouped
+    T.PParens pat' -> groupMatches' (T.Match pat' body : rest) grouped
