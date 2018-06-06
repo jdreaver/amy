59abf2b87bb3c7768143f76bee19a080ea213aa9
David Reaver <johndreaver@gmail.com>
2018-06-06 16:40:48 -0700

    Nuke Renamer since it isn't needed

--- library/Amy/Errors.hs
+++ library/Amy/Errors.hs
@@ -11,23 +11,12 @@ import Text.Groom
 import Text.Megaparsec
 
 import Amy.Kind
-import Amy.Renamer.AST as R
 import Amy.Syntax.AST as S
 import Amy.TypeCheck.AST as T
 
 data Error
   -- Parser
   = ParserError !(ParseError Char Void)
-
-  -- Renamer
-  | UnknownVariable !(Located IdentName)
-  | UnknownDataCon !(Located DataConName)
-  | VariableShadowed !(Located IdentName) !(Located IdentName)
-  | DuplicateDataConstructorName !(Located DataConName) !R.DataConDefinition
-  | DuplicateTypeVariable !(Located TyVarName) !(Located TyVarName)
-  | TypeConstructorAlreadyExists !(Located TyConName)
-  | UnknownTypeConstructor !(Located TyConName)
-  | UnknownTypeVariable !(Located TyVarName)
 
   -- Type checker
   -- TODO: Add source spans here
@@ -44,27 +33,12 @@ data Error
   | InfiniteType !TyExistVarName !T.Type
   | InfiniteKind !Int !Kind
   | TooManyBindingArguments !S.Binding
-
-  -- | BindingLacksTypeSignature !RBinding
-  -- | TypeMismatch !(Type PrimitiveType) !(Type PrimitiveType)
-  -- | CantFindType !(Located Name)
-  -- | WrongNumberOfArguments !Int !Int
-  -- | ExpectedPrimitiveType !(Maybe (Located Name)) !(Type PrimitiveType)
-  -- | ExpectedFunctionType !(Type PrimitiveType)
   deriving (Show, Eq)
 
 errorLocation :: Error -> Maybe SourceSpan
 errorLocation e =
   case e of
     ParserError{} -> Nothing
-    UnknownVariable (Located s _) -> Just s
-    UnknownDataCon (Located s _) -> Just s
-    VariableShadowed (Located s _) _ -> Just s
-    DuplicateDataConstructorName (Located s _) _ -> Just s
-    DuplicateTypeVariable (Located s _) _ -> Just s
-    TypeConstructorAlreadyExists (Located s _) -> Just s
-    UnknownTypeConstructor (Located s _) -> Just s
-    UnknownTypeVariable (Located s _) -> Just s
 
     UnknownVariable'{} -> Nothing
     UnknownDataCon'{} -> Nothing
@@ -79,13 +53,6 @@ errorLocation e =
     InfiniteType{} -> Nothing
     InfiniteKind{} -> Nothing
     TooManyBindingArguments{} -> Nothing
-
-    -- BindingLacksTypeSignature bind -> Just $ locatedSpan $ rBindingName bind
-    -- TypeMismatch{} -> Nothing
-    -- CantFindType{} -> Nothing
-    -- WrongNumberOfArguments{} -> Nothing
-    -- ExpectedPrimitiveType mLocated _ -> locatedSpan <$> mLocated
-    -- ExpectedFunctionType{} -> Nothing
 
 showError :: Error -> String
 showError (ParserError err) = parseErrorPretty err
